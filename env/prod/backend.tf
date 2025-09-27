613708f7a81b0030b81fc1b354c8a57ce5f6b59f
vikramazure <vikramazure02@robinson15kumargmail.onmicrosoft.com>
2025-09-27 13:01:34 +0000

    Updated backend.tf

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "azurerm" {
     access_key           = "KyZfhDVlbETYorG/kmtwhAgHkErpusX+NfD1F27or08HYXB/Huzd0+5e/PECyr/vI11vtlEiVNm4+AStUjjxXA==" 
-    resourc_group_name = todo-rg1 # Can also be set via `ARM_ACCESS_KEY` environment variable.
     storage_account_name = "azurepipeline123"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "azure"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
